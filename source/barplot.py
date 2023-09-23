f067d852bdea248dca54b83fe76533999f830f65
Radovan Bast <bast@users.noreply.github.com>
2023-09-23 20:45:40 +0200

    simplify

--- source/barplot.py
+++
@@ -1,9 +0,0 @@
-# plot bar chart with Matplotlib
-from collections import Counter
-import matplotlib.pyplot as plt
-
-sales = Counter(banana=15, tomato=4, apple=39, orange=30).most_common()
-x, y = zip(*sales)
-plt.bar(x, y)
-plt.show()
-
--- source/mode.py
+++
@@ -1,8 +0,0 @@
-# mode.py
-from collections import Counter
-
-def mode(data):
-  counter = Counter(data)
-  _, top_count = counter.most_common(1)[0]
-  return [point for point, count in counter.items() if count == top_count]
-
